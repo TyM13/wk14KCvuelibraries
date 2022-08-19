acb7e3269a650a962c5f47701252478cecb45ef6
Your Name <you@example.com>
2022-08-18 19:31:19 -0600

    vue libraries wk13 knowledgecheck

---
+++ src/components/AllUsers.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="(data, index) in data" :key="index">
+      {{ data[`data`][`first_name`] }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  mounted() {
+    axios
+      .request({
+        url: `https://reqres.in/api/users`,
+      })
+      .then((response) => {
+        response;
+      })
+      .catch((error) => {
+        error;
+      });
+  },
+};
+</script>
+
+<style scoped>
+</style>
